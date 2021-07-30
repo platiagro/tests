228180488c07ef40c1e24bdfc3b58f7480174d0e
emelo <emelo@cpqd.com.br>
2021-07-30 20:07:50 -0300

    Upgrade

--- cucumber-geb/src/cucumber/resources/steps/PlatIAgro/CRUD_Experimento/Experimento_Erro.groovy
+++ cucumber-geb/src/cucumber/resources/steps/PlatIAgro/CRUD_Experimento/Experimento_Erro.groovy
@@ -195,6 +195,8 @@ E(/adicionar a tarefa Regressão Linear ao fluxo/) { ->
   JavascriptExecutor jse = (JavascriptExecutor)browser.driver;
   jse.executeScript("function createEvent(typeOfEvent) {\n" +"var event =document.createEvent(\"CustomEvent\");\n" +"event.initCustomEvent(typeOfEvent,true, true, null);\n" +"event.dataTransfer = {\n" +"data: {},\n" +"setData: function (key, value) {\n" +"this.data[key] = value;\n" +"},\n" +"getData: function (key) {\n" +"return this.data[key];\n" +"}\n" +"};\n" +"return event;\n" +"}\n" +"\n" +"function dispatchEvent(element, event,transferData) {\n" +"if (transferData !== undefined) {\n" +"event.dataTransfer = transferData;\n" +"}\n" +"if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n" +"} else if (element.fireEvent) {\n" +"element.fireEvent(\"on\" + event.type, event);\n" +"}\n" +"}\n" +"\n" +"function simulateHTML5DragAndDrop(element, destination) {\n" +"var dragStartEvent =createEvent('dragstart');\n" +"dispatchEvent(element, dragStartEvent);\n" +"var dropEvent = createEvent('drop');\n" +"dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n" +"var dragEndEvent = createEvent('dragend');\n" +"dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" +"}\n" +"\n" +"var source = arguments[0];\n" +"var destination = arguments[1];\n" +"simulateHTML5DragAndDrop(source,destination);",source, destination);
 
+  Thread.sleep(1000)
+
   waitFor(30) {
     $(By.xpath("//*[@id='root']/section/section/section/main/section/main/div[1]/div[1]/div[1]/button[3]/span")).click()
   }
