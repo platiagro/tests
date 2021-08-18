188299d1c4edf147b9a63417d35768d4be801305
emelo <emelo@cpqd.com.br>
2021-08-18 09:12:57 -0300

    Upgrade

--- Jenkinsfile-GEB.groovy
+++ Jenkinsfile-GEB.groovy
@@ -34,7 +34,7 @@ pipeline {
         string(
             name: 'GRADLE_BDD_PARAM',
             description: "Parâmetros para a chamada do 'gradle bdd'",
-            defaultValue: '-Ptag=@CRUDPROJ@CRUDEXP@FLUXOIMP -Penv=chromeheadless',
+            defaultValue: '-Ptag=@CRUDPROJ@CRUDTAREFA@CRUDEXP@FLUXOIMP -Penv=chromeheadless',
             trim: true,
         )               
         string(
