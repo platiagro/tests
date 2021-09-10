cb3f1e77542c3b758eafc971c446a8f5965e6f5a
emelo <emelo@cpqd.com.br>
2021-09-10 12:46:11 -0300

    Upgrade

---
+++ cucumber-geb/config/codenarc/rules.groovy
@@ -0,0 +1,16 @@
+ruleset {
+    description 'Project RuleSet'
+
+    ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/convention.xml')
+    ruleset('rulesets/design.xml')
+    ruleset('rulesets/dry.xml')
+    ruleset('rulesets/exceptions.xml')
+    ruleset('rulesets/formatting.xml')
+    ruleset('rulesets/generic.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/naming.xml')
+    ruleset('rulesets/unnecessary.xml')
+    ruleset('rulesets/unused.xml')
+}
