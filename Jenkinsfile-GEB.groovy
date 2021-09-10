877fc9afdba8f74a040751e89c6b0e5ac4276736
emelo <emelo@cpqd.com.br>
2021-09-10 14:36:54 -0300

    Upgrade

--- Jenkinsfile-GEB.groovy
+++ Jenkinsfile-GEB.groovy
@@ -117,18 +117,18 @@ pipeline {
                         gradle check
                     """
                 }
-            }
-            echo 'Publish Codenarc report'
-            publishHTML(
-                target: [
+                echo 'Publish Codenarc report'
+                publishHTML(
+                    target:
                         allowMissing         : false,
                         alwaysLinkToLastBuild: false,
                         keepAll              : true,
                         reportDir            : 'target/site',
                         reportFiles          : 'codenarc.html',
                         reportName           : "Codenarc Report"
-                ]
-            )
+                    ]
+                )
+            }
         }
         
         stage('GEB: Execução dos testes') {     
